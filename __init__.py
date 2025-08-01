7cc28f95f749863847b2b8225b60a73b45ecc70d
andrewarnaud1 <113008496+andrewarnaud1@users.noreply.github.com>
2025-08-01 12:05:06 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,4 +0,0 @@
-"""Imports des utilitaires pour les scénarios techniques."""
-from .cliquer import cliquer
-from .ecrire import ecrire
-from .trouver_coordonnees_image import trouver_coordonnees_image
